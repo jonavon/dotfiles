5250947ba1258f48b026726139b9f5a4dba961b3
Jonavon Wilcox <jonavon.l.wilcox.ctr@nga.mil>
2024-04-15 09:18:43 -0400

    Update setup script

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+shopt -s expand_aliases
+
+# Create an alias for the Git command
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+
 # Function to check if the dotfiles repository is already set up
 is_repo_setup() {
 	if [ -d "$HOME/.cfg" ]; then
@@ -19,20 +26,18 @@ if is_repo_setup; then
 else
 	echo "Before we begin; Let's make sure you have your email set"
 
-	read -t 3 -n 1 -p "Is your email set in git global config (Y/n)?" answer
-	[ -z "$answer" ] && answer="N"
+set +e
+	read -t 7 -n 1 -p "Is your email set in git global config (Y/n)?" answer
+set -e
 	if [[ $answer == [Nn] ]]
 		then
 			git config --global --edit	
 	fi
-
 	echo "Setting up dotfiles repository..."
 
 	# Initialize a bare Git repository for the dotfiles
 	git init --bare $HOME/.cfg
 
-	# Create an alias for the Git command
-	alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 	# Set local Git configuration to not show untracked files
 	dotfiles config --local status.showUntrackedFiles no
@@ -47,9 +52,19 @@ else
 	dotfiles config --local core.sparseCheckout true
 
 	dotfiles config --local alias.config "config --local"
-
+	
+	dotfiles branch -m main
+	
 	# Add the alias to the bashrc file
-	echo "alias dotfiles='/usr/bin/git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'" >> $HOME/.bashrc
+
+set +e
+	read -t 7 -n 1 -p "Would you like to add the 'dotfiles' alias to your '.bashrc' file? (y/N)?" addalias
+set -e
+	if [[ $addalias == [Yy] ]]; then
+		echo "alias dotfiles='/usr/bin/git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'" >> $HOME/.bashrc
+		echo "Added the alias to .bashrc"
+		tail -n 3 "$HOME/.bashrc"
+	fi
 
 	# Change master to main
 	dotfiles branch -m main
@@ -59,12 +74,13 @@ else
 	dotfiles commit --allow-empty -m "Initial commit"
 
 	# Create a new branch
-	read -t 5 -p "What branchname do you want to use [${HOME##*/}]?" branch
+set +e
+	read -t 30 -p "What branchname do you want to use [${HOME##*/}]?" branch
+set -e
 	
 	dotfiles checkout HEAD -b ${branch:=${HOME##*/}}
 	
 fi
-
 # Ignore sensitive files and directories in .cfg/info/exclude
 if ! grep -q "/.gnupg" "$HOME/.cfg/info/exclude"; then
 	echo "/.gnupg" >> $HOME/.cfg/info/exclude
